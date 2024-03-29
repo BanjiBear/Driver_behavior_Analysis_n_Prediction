15244f460805d7c02586fed51c9a838c41a0191f
BanjiBear <tommychen900217@gmail.com>
2024-03-29 18:19:37 +0800

    [Code] Big Update on data
    Successfully load data in Spark Dataset, Done Data Preprocessing(so they fit 19 columns 18 commas) and assign column names

--- src/main/java/hk/polyu/webservice/spark/DBAP/config/AppConfig.java
+++ src/main/java/hk/polyu/webservice/spark/DBAP/config/AppConfig.java
@@ -1,8 +1,14 @@
 package hk.polyu.webservice.spark.DBAP.config;
 
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 //Spring dependency
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +20,7 @@ import org.springframework.context.annotation.Configuration;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import static org.apache.spark.sql.functions.col;
 
 
 // Root Configuration
@@ -32,11 +39,34 @@ public class AppConfig {
 	
 	@Value("${spring.application.name}")
 	private String applicationName;
-	
 	@Value("${spring.application.data.path}")
 	private String dataPath;
 	
-	private ArrayList<Dataset<Row>> data = new ArrayList<Dataset<Row>>();
+	private List<Dataset<Row>> data = new ArrayList<Dataset<Row>>();
+	private static final List<String> columnName = Collections.unmodifiableList( new ArrayList<String>() {
+		{
+	        add("driverID");
+	        add("carPlateNumber");
+	        add("latitude");
+	        add("longtitude");
+	        add("speed");
+	        add("direction");
+	        add("siteName");
+	        add("time");
+	        add("isRapidlySpeedup");
+	        add("isRapidlySlowdown");
+	        add("isNeutralSlide");
+	        add("isNeutralSlideFinished");
+	        add("neutralSlideTime");
+	        add("isOverspeed");
+	        add("isOverspeedFinished");
+	        add("overspeedTime");
+	        add("isFatigueDriving");
+	        add("isHthrottleStop");
+	        add("isOilLeak");
+	    }
+	} );
+	Integer NUMBER_OF_FEATURES = 19;
 	
 	@Bean
 	public SparkSession sparkSession(){
@@ -52,14 +82,74 @@ public class AppConfig {
 	}
 	
 	@Bean
-	public ArrayList<Dataset<Row>> getData(){
+	public List<Dataset<Row>> getData(){
 		SparkSession sparkSession = this.sparkSession();
 		File[] allDataFiles = new File(dataPath).listFiles();
-		
+		dataPreProcess(allDataFiles);
 		for(int i = 0; i < allDataFiles.length; i++){
-			this.data.add(sparkSession.read().csv(dataPath + allDataFiles[i].getName()));
-		}		
+			Dataset<Row> dataset = sparkSession.read().csv(dataPath + allDataFiles[i].getName());
+			this.data.add(this.updateColumnName(dataset));
+		}	
 		return this.data;
+	}
+	
+	private Dataset<Row> updateColumnName(Dataset<Row> dataset) {
+		for(int i = 0; i < this.columnName.size(); i++) {
+			dataset = dataset.withColumnRenamed("_c" + Integer.toString(i), AppConfig.columnName.get(i));
+		}
+		dataset = dataset.drop("_c" + Integer.toString(19));
+		return dataset;
+	}
+	
+	
+	private void dataPreProcess(File[] allDataFiles) {
+		//40440, 38931, 56715, 30801, 40494, 36082, 36856, 44499, 42787, 45845
+		/*
+		 * Reference:
+		 * https://stackoverflow.com/questions/202148/replace-first-line-of-a-text-file-in-java#:~:text=First%20use%20BufferedReader%20's%20readLine,the%20copy%20line%20by%20line.
+		 * https://stackoverflow.com/questions/20039980/java-replace-line-in-text-file
+		 * */
+		for(int i = 0; i < allDataFiles.length; i++){
+			try {
+				// input the file content to the StringBuffer "input"
+				BufferedReader file = new BufferedReader(new FileReader(dataPath + allDataFiles[i].getName()));
+				StringBuffer inputBuffer = new StringBuffer();
+				String line;
+				
+				while ((line = file.readLine()) != null) {
+					int count = 0;
+					for (int j = 0; j < line.length(); j++) {
+					    if (line.charAt(j) == ',') {
+					        count++;
+					    }
+					}
+					if(count < NUMBER_OF_FEATURES - 1) {
+						for(int j = 0; j < (NUMBER_OF_FEATURES - 1 - count); j++) {
+							line = line + ',';
+						}
+					}
+					inputBuffer.append(line);
+					inputBuffer.append('\n');
+				}
+				file.close();
+				
+				String inputStr = inputBuffer.toString();				
+				
+				// write the new string with the replaced line OVER the same file
+				FileOutputStream fileOut = new FileOutputStream(dataPath + allDataFiles[i].getName());
+				fileOut.write(inputStr.getBytes());
+				fileOut.close();
+				
+				file = null;
+				inputBuffer = null;
+				line = null;
+				inputStr = null;
+				fileOut = null;
+				System.gc();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
 
